f543ff3400c966660eec742916db99abd9712d3a
AReasonWhy <pharotek.jesod@gmail.com>
2017-01-16 19:48:44 +0100

    2.0  kreirane klase za zadatke

---
+++ src/main/java/model/Knjiga.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created by androiddevelopment on 16.1.17..
+ */
+public class Knjiga {
+    private int id;
+    private String naslov;
+    private String BrojStrana;
+    private java.util.Date datumIzdanja;
+    private boolean pristuna;
+}
